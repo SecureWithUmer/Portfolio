7d2e4b860a6375668f189fbf6ed91acc90ef79ac
Mr Black Bear <mrblackbear8@gmail.com>
2025-06-15 18:46:14 +0000

    The picture location has to be permanent just below the menu border. So

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -73,7 +73,7 @@ export default function HomePage() {
 
   return (
     <div className="space-y-16">
-      <section className="text-center pt-2 pb-16 sm:pt-4 md:pt-6 min-h-[70vh] flex flex-col justify-start items-center overflow-hidden">
+      <section className="text-center pt-0 pb-16 min-h-[70vh] flex flex-col justify-start items-center overflow-hidden">
         <motion.div
           initial={{ opacity: 0, scale: 0.8 }}
           animate={{ opacity: 1, scale: 1 }}
@@ -171,7 +171,7 @@ export default function HomePage() {
               }}
               className="cursor-pointer"
             >
-              <Card className="flex flex-col items-center"> {/* Removed h-full here */}
+              <Card className="flex flex-col items-center">
                 <CardHeader className="pb-4 w-full">
                   <motion.div
                     className="inline-block p-2"
