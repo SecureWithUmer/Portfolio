25cc00071eb83d0f632b130d97d854827b666e61
Mr Black Bear <mrblackbear8@gmail.com>
2026-02-11 20:01:20 +0000

    put tryhackme link and icon in social links https://tryhackme.com/p/secu

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -25,12 +25,32 @@ const WhatsAppIcon = (props: React.SVGProps<SVGSVGElement>) => (
     </svg>
 );
 
+const TryHackMeIcon = (props: React.SVGProps<SVGSVGElement>) => (
+    <svg
+        xmlns="http://www.w3.org/2000/svg"
+        width="24"
+        height="24"
+        viewBox="0 0 24 24"
+        fill="none"
+        stroke="currentColor"
+        strokeWidth="2"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        {...props}
+    >
+        <path d="M12 2L2 6v5c0 5.55 3.84 10.74 9 12 5.16-1.26 9-6.45 9-12V6L12 2z" />
+        <path d="M9 10h6" />
+        <path d="M12 10v7" />
+    </svg>
+);
+
 
 const socialLinks = [
   { name: 'LinkedIn', href: 'https://www.linkedin.com/in/hackandsecurewithumer', icon: Linkedin },
   { name: 'GitHub', href: 'https://github.com/SecureWithUmer', icon: Github },
   { name: 'Email', href: 'mailto:hackwithumer@outlook.com', icon: Mail },
   { name: 'WhatsApp', href: 'https://wa.me/923261149625', icon: WhatsAppIcon },
+  { name: 'TryHackMe', href: 'https://tryhackme.com/p/securewithumer', icon: TryHackMeIcon },
 ];
 
 const StatusBar = () => {
