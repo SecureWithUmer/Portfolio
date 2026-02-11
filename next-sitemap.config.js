e46f86e30e18b1cb80e13ec11e2d0d89d3e07393
Mr Black Bear <mrblackbear8@gmail.com>
2026-02-11 19:02:13 +0000

    Ensure you have a public/robots.txt file. It should look like this:
    Pla

---
+++ next-sitemap.config.js
@@ -0,0 +1,5 @@
+/** @type {import('next-sitemap').IConfig} */
+module.exports = {
+  siteUrl: 'https://securewithumer.vercel.app',
+  generateRobotsTxt: false, // We are using a custom robots.txt file
+};
